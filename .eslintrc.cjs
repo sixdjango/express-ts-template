dfbe088e9313559ae9765dad13036a64c6845713
django <six.django@gmail.com>
2024-09-11 11:04:37 +0800

    feat: update dependency

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ['@yc-tech/eslint-config'].map(require.resolve),
+
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-var': 'off'
+  }
+}
